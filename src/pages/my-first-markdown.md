3271fffb58ba1f7ea886cf834b2cd4e42c60e13f
Nic Lehman <53277092+wisconic@users.noreply.github.com>
2023-11-02 17:01:37 -0500

    tutorial - basics

---
+++ src/pages/my-first-markdown.md
@@ -0,0 +1,3 @@
+# My First Markdown
+
+This is a markdown page added by me.
---
+++ src/pages/my-first-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyFirstPage() {
+  return (
+    <Layout>
+      <h1>My First page</h1>
+      <p>This is a my first page added.</p>
+    </Layout>
+  );
+}
